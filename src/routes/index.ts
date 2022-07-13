51f7c891ab996a07e3de1254c0351589ab0320d0
yuregl <yuregaldino@hotmail.com>
2022-07-12 21:13:44 -0300

    feat: added Users routes in the app

---
+++ src/routes/index.ts
@@ -0,0 +1,33 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import cors from "cors";
+
+import { createUsersRoutes } from "./UsersRoutes"
+
+async function exportApp() {
+  const app = express();
+  app.use(morgan("dev"));
+  app.use(cors());
+  app.use(express.json());
+  app.use(createUsersRoutes());
+  app.use(
+    // eslint-disable-next-line no-unused-vars
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof Error) {
+        return response.status(400).json({
+          error: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    }
+  );
+
+  return app;
+}
+
+export { exportApp };
