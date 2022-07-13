ab16baa20676910ec3349b766b5f41292b81cd9d
yuregl <yuregaldino@hotmail.com>
2022-07-12 21:09:51 -0300

    feat: create repositories Users

---
+++ src/repositories/Users.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Users } from "../models/Users";
+
+@EntityRepository(Users)
+class UsersRepository extends Repository<Users> {}
+
+export { UsersRepository };
