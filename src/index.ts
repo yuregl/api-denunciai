3e23dfba55965c2b10578b991ffbe9e9fdd95e0f
yuregl <yuregaldino@hotmail.com>
2022-07-12 21:16:49 -0300

    feat: added file starting server

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata"
+import dotenv from "dotenv";
+
+import { exportApp } from './routes/index';
+import { startDatabase } from './database/index';
+
+dotenv.config();
+
+async function start() {
+  await startDatabase();
+  const port = process.env.PORT;
+  const app =  await exportApp();
+
+  app.listen(port, () => {
+    console.log(`Server is running in port http://localhost:${port}`);
+  })
+}
+
+start()
+
