cecf3d607e6151324a49cc821d11850ef9f6ee2c
yuregl <yuregaldino@hotmail.com>
2022-07-12 21:05:00 -0300

    feat: create functions encrytp and compare hash

---
+++ src/utils/EncryptPassword.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt';
+
+export async function hashPassword(password: string):Promise<string>{
+  const salt = parseInt(<string>process.env.SALT);
+  return await bcrypt.hash(password, salt);
+}
+
+export async function comparePassword(password: string, hashedPassword: string): Promise<boolean>{
+  return await bcrypt.compare(password, hashedPassword)
+}
