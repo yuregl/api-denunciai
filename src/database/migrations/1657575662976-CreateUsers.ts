272c7902571cb37d98edcd4b118f6bdeabf72628
yuregl <yuregaldino@hotmail.com>
2022-07-12 21:07:16 -0300

    feat: added connection database and migrations

---
+++ src/database/migrations/1657575662976-CreateUsers.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1657575662976 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar"
+          },
+          {
+            name: "full_name",
+            type: "varchar"
+          },
+          {
+            name: "email",
+            type: "varchar"
+          },
+          {
+            name: "password",
+            type: "varchar"
+          },
+          {
+            name: "admin",
+            type: "boolean",
+            default: false
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "active",
+            type: "boolean",
+            default: true
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users")
+  }
+
+}
