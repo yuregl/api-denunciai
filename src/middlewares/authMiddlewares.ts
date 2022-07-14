fed0b1fc8f4d215c0b8fc65abc49c86da29af91d
yuregl <yuregaldino@hotmail.com>
2022-07-13 21:51:41 -0300

    feat: create authMiddlewares

---
+++ src/middlewares/authMiddlewares.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction} from 'express';
+import jwt from "jsonwebtoken"
+
+interface ITokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+  if(!authorization) {
+    return res.sendStatus(401)
+  }
+
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+    const data = jwt.verify(token, <string> process.env.SECRET);
+    const { id } = data as ITokenPayload;
+    req.id = id;
+    return next();
+  } catch {
+    res.sendStatus(401);
+  }
+
+}
