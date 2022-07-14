69e275d3abb2996067185cbc0b2dbb35d1636fc1
yuregl <yuregaldino@hotmail.com>
2022-07-13 22:03:51 -0300

    feat: added auth in the deleteAccount route

--- src/routes/UsersRoutes.ts
+++ src/routes/UsersRoutes.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { UsersController } from "../controllers/UserController";
 import { UsersService } from "../services/UsersServices";
 import { UsersRepository } from "../repositories/Users";
+import authMiddleware from "../middlewares/authMiddlewares";
 
 const userRoutes = Router();
 
@@ -11,7 +12,11 @@ function createUsersRoutes() {
   const userService = new UsersService(userRepository);
   const userController = new UsersController(userService);
 
-  userRoutes.post("/user/new", userController.handleCreateUser)
+  userRoutes.post("/user/new", userController.handleCreateUser);
+
+  userRoutes.get("/user/login", userController.handleLogin)
+
+  userRoutes.delete('/user/delete/:uuid', authMiddleware ,userController.handleDeleteAccount)
 
   return userRoutes;
 }
