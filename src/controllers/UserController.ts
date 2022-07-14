d7ba2d498ac83e9a18bdecd8d70aeea80d620048
yuregl <yuregaldino@hotmail.com>
2022-07-13 22:08:58 -0300

    feat: added deleteAccoount

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { json, Request, Response } from 'express';
 import { UsersService } from '../services/UsersServices';
 
 class UsersController {
@@ -6,12 +6,23 @@ class UsersController {
 
   handleCreateUser = async (request: Request, response: Response) => {
     const user = request.body;
-    console.log("USER: ",user)
-
     await this.userService.executeCreateUser(user)
-
     return response.status(201).json({message: "User created successfully"})
   }
+
+  handleLogin = async (request: Request, response: Response) => {
+    const { email, password} = request.body;
+    const token = await this.userService.executeLogin({email, password});
+    return response.status(200).json(token)
+  }
+
+  handleDeleteAccount = async(request: Request, response: Response) => {
+    const { id } = request;
+    const { uuid } = request.params;
+    await this.userService.executeDeleteAccount({ id, uuid})
+    return response.status(200).json({message: "User deleted"});
+  }
+  
 }
 
 export { UsersController }
