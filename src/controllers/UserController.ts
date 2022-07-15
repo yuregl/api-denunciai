c582d02d38e4db0b23360ffe2604f8a6683d57c7
yuregl <yuregaldino@hotmail.com>
2022-07-14 21:36:59 -0300

    feat: added updateUser controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,13 @@ import { Request, Response } from 'express';
 import { UsersService } from '../services/UsersServices';
 import { validationResult } from 'express-validator';
 
+interface IUserUpdate {
+  name?: string;
+  full_name?: string;
+  password?: string;
+  admin?: boolean;
+}
+
 class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -52,7 +59,22 @@ class UsersController {
     const { id } = request;
     const { uuid } = request.params;
     await this.userService.executeDeleteAccount({ id, uuid})
-    return response.status(200).json({message: "User deleted"});
+    return response.status(200).json({message: "Deleted User"});
+  }
+
+  handleUpdateAccount = async(request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if(!errors.isEmpty()){
+      return response.status(400).json({errors: errors.array()})
+    }
+
+    const user = request.body as IUserUpdate ;
+    const { uuid } = request.params;
+
+    await this.userService.executeUpdateUser(user, uuid as string);
+
+    return response.status(200).json({message: "Update user"})
   }
   
 }
