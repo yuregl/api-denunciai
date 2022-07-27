0f9231ed66985e77a5d091b59de77d91ad0164ab
yuregl <yuregaldino@hotmail.com>
2022-07-27 17:26:13 -0300

    fix: added implementation conditional

--- src/middlewares/parseRequest.ts
+++ src/middlewares/parseRequest.ts
@@ -4,7 +4,7 @@ const parseRequestComplaintsCreate = function (req: Request, res: Response, next
   const parseValue = JSON.parse(req.body.body)
   const dateImages = `${Date.now()}`
   parseValue.dateImages = dateImages;
-  if(!parseValue.userId){
+  if(!parseValue.userId || req.files.length === 0){
     return res.sendStatus(400).json({message: "Unsent data"})
   }
   req.body = parseValue;
