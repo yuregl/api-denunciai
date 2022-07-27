dffb86eec150a31d34b5c3c8afd2f157ca98273c
yuregl <yuregaldino@hotmail.com>
2022-07-27 17:27:47 -0300

    feat: implementation ComplaintsController

--- src/controllers/ComplaintsController.ts
+++ src/controllers/ComplaintsController.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from 'express';
-import { ComplaintsService } from 'services/ComplaintsServices';
+import { ComplaintsService } from '../services/ComplaintsServices';
 import { FilesService } from '../services/FilesServices';
 
 class ComplaintsController {
   constructor(
     private complaintsService: ComplaintsService,
     private filesService: FilesService
-    ){}
+  ){}
+
   handleCreateComplaints = async (request: Request, response: Response) => {
-    await this.filesService.executeCreateFiles(request.files)
-    await this.complaintsService.executeCreateComplaints(request.body)
+    const { id } = await this.complaintsService.executeCreateComplaints(request.body)
+    await this.filesService.executeCreateFiles(request.files, id, request.body.userId);
     return response.status(201).json({message: "Complaint created successfully"})
   }
+
 }
 
 export { ComplaintsController }
