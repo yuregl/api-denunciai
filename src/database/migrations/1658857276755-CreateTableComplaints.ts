14fee9d8acaba153c092fd8d185839c91599b088
yuregl <yuregaldino@hotmail.com>
2022-07-27 00:00:18 -0300

    feat: added migration complaints

---
+++ src/database/migrations/1658857276755-CreateTableComplaints.ts
@@ -0,0 +1,64 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTableComplaints1658857276755 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "complaints",
+        columns:[
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "user_id",
+            type: "uuid"
+          },
+          {
+            name: "title",
+            type: "varchar"
+          },
+          {
+            name: "description",
+            type: "varchar"
+          },
+          {
+            name: "status",
+            type: "varchar"
+          },
+          {
+            name: "address",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ],
+        foreignKeys: [
+          {
+            name: "FKUserComplaints",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL"
+          }
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("complaints")
+  }
+
+}
