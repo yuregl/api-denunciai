9d640a2d7382b4f2b3f4b872029267d24d1a5668
yuregl <yuregaldino@hotmail.com>
2022-07-31 11:32:39 -0300

    feat: added getComplaintById controller

--- src/controllers/ComplaintsController.ts
+++ src/controllers/ComplaintsController.ts
@@ -14,12 +14,24 @@ class ComplaintsController {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()){
-      return response.status(400).json({errors: errors.array()})
+      return response.status(400).json({errors: errors.array()});
     }
     
-    const { id } = await this.complaintsService.executeCreateComplaints(request.body)
+    const { id } = await this.complaintsService.executeCreateComplaints(request.body);
     await this.filesService.executeCreateFiles(request.files, id, request.body.userId);
-    return response.status(201).json({message: "Complaint created successfully"})
+    return response.status(201).json({message: "Complaint created successfully"});
+  }
+
+  handleGetComplaintById = async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if(!errors.isEmpty()){
+      return response.status(400).json({errors: errors.array()});
+    }
+    
+    const complaintId = request.params.complaint_id;
+    const result = await this.complaintsService.executeGetComplaintById(complaintId);
+    return response.json(result);
   }
 
 }
