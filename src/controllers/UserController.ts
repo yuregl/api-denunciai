1de08778d4327547d8863dbd6543f61eb39fdaa1
yuregl <yuregaldino@hotmail.com>
2022-07-31 11:28:38 -0300

    styles: adding the semicolons

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -16,24 +16,24 @@ class UsersController {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()){
-      return response.status(400).json({errors: errors.array()})
+      return response.status(400).json({errors: errors.array()});
     }
 
     const user = request.body;
-    await this.userService.executeCreateUser(user)
-    return response.status(201).json({message: "User created successfully"})
+    await this.userService.executeCreateUser(user);
+    return response.status(201).json({message: "User created successfully"});
   }
 
   handleLogin = async (request: Request, response: Response) => {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()){
-      return response.status(400).json({errors: errors.array()})
+      return response.status(400).json({errors: errors.array()});
     }
     
     const { email, password} = request.body;
     const token = await this.userService.executeLogin({email, password});
-    return response.status(200).json(token)
+    return response.status(200).json(token);
   }
 
   handleGetUser = async(request: Request, response: Response) => {
@@ -45,20 +45,20 @@ class UsersController {
 
     const { uuid } = request.headers;
 
-    const user = await this.userService.executeGetUser(uuid as string)
-    return response.status(200).json(user)
+    const user = await this.userService.executeGetUser(uuid as string);
+    return response.status(200).json(user);
   }
 
   handleDeleteAccount = async(request: Request, response: Response) => {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()){
-      return response.status(400).json({errors: errors.array()})
+      return response.status(400).json({errors: errors.array()});
     }
     
     const { id } = request;
     const { uuid } = request.params;
-    await this.userService.executeDeleteAccount({ id, uuid})
+    await this.userService.executeDeleteAccount({ id, uuid});
     return response.status(200).json({message: "Deleted User"});
   }
 
@@ -66,15 +66,15 @@ class UsersController {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()){
-      return response.status(400).json({errors: errors.array()})
+      return response.status(400).json({errors: errors.array()});
     }
 
-    const user = request.body as IUserUpdate ;
+    const user = request.body as IUserUpdate;
     const { uuid } = request.params;
 
     await this.userService.executeUpdateUser(user, uuid as string);
 
-    return response.status(200).json({message: "Update user"})
+    return response.status(200).json({message: "Update user"});
   }
   
 }
