3ff83361af5b2ed00c529610696f4e5709993120
yuregl <yuregaldino@hotmail.com>
2022-08-05 12:58:33 -0300

    feat: added query getComplaintsByIdAndUserId

--- src/repositories/Files.ts
+++ src/repositories/Files.ts
@@ -1,8 +1,19 @@
 
 import { EntityRepository, Repository } from "typeorm";
 import { Files } from "../models/Files";
+import { IListSave } from "../interfaces/saveS3"
 
 @EntityRepository(Files)
-class FilesRepository extends Repository<Files> {}
+class FilesRepository extends Repository<Files> {
+  async getComplaintsByIdAndUserId(userId: string, complaintId: string): Promise<Array<IListSave>> {
+    return await this.query(`
+    SELECT url, key FROM files
+    INNER JOIN complaints ON files.complaints_id  = complaints.id 
+    INNER JOIN users ON complaints.user_id  = users.id
+    AND users.id = '${userId}'
+    WHERE complaints_id = '${complaintId}'
+  `) as Array<IListSave>;
+  }
+}
 
 export { FilesRepository };
